b548ac54a8683360554787ea15bdb9b94e0989a1
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-04-07 11:24:53 -0300

    EP125

---
+++ MVC/ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,22 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers;
+
+public class CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+    : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Crear()
+    {
+        var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+        var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+        var modelo = new CuentaCreacionViewModel();
+
+        modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+        
+        return View(modelo);
+    }
+}
---
+++ MVC/ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models;
+
+public class CuentaCreacionViewModel : Cuenta
+{
+    public IEnumerable<SelectListItem>? TiposCuentas { get; set; }
+}
